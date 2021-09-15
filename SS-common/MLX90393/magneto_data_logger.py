2a5fb4781febd00f25bb34f769212b2636b1d008
WojciechSadzik <wojciech.sadzik@altenpolska.pl>
2021-09-15 17:52:01 +0200

    Add some documentation to the python script

--- SS-common/MLX90393/magneto_data_logger.py
+++ SS-common/MLX90393/magneto_data_logger.py
@@ -6,6 +6,7 @@ COM_PORT = 'COM4'
 BAUDRATE = 115200
 TIMEOUT = 10
 
+# Try to create an serial port instance
 try:
     magneto = serial.Serial(COM_PORT, baudrate=BAUDRATE, timeout=TIMEOUT)
     print("Serial port opened successfully\n")
@@ -14,10 +15,19 @@ except SerialException:
     exit()
 
 def magneto_read_lines():
-    data = magneto.readline() # read one full line
+    """Read one full line (terminated with \n) from Kromek"""
+
+    data = magneto.readline()
     return data
 
 def parse_magneto_data(data):
+    """
+    Parse raw data received from Kromek
+    Example: 
+    input - b'\x31\x32\x33' (bytes)
+    output - 1 2 3 (string)
+    """
+
     parsed_data = str(data).split("b'")
     parsed_data = parsed_data[1].split("\\n'")
     return parsed_data[0]
@@ -25,7 +35,7 @@ def parse_magneto_data(data):
 # TODO: Add support for multiple (11) magnetometers
 def main():
     with open("magneto_data.txt", "w") as file:
-        # Write header
+        # Write header into the file
         file.write("X1\t  Y1\tZ1\n")
 
         # Get the data and save it to the file
